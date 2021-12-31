fc7db8b1e5376182435fad19fdfacbcdfe758adc
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-12-31 12:56:55 -0500

    install tkinter and applied minimal changes for now

---
+++ app.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=500, width=500, bg="#6c25be")
+canvas.pack()
+
+root.mainloop()

129a23fc754d498b595d471a080c66e87ac9d75e
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-12-31 13:24:43 -0500

    added buttons

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ import os
 
 root = tk.Tk()
 
-canvas = tk.Canvas(root, height=500, width=500, bg="#6c25be")
+canvas = tk.Canvas(root, height=500, width=500, bg="#8960d8")
 canvas.pack()
+
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#8960d8")
+openFile.pack()
+
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#8960d8")
+runApps.pack()
 
 root.mainloop()

b69566c678e26bc0b4419baba9e0ace3473bfb8a
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-12-31 14:29:23 -0500

    apps save to array and open when Run App button clicked

--- app.py
+++ app.py
@@ -3,14 +3,44 @@ from tkinter import filedialog, Text
 import os
 
 root = tk.Tk()
+apps = []
 
-canvas = tk.Canvas(root, height=500, width=500, bg="#8960d8")
+def addApp():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+    filename = filedialog.askopenfilename(
+        initialdir="/", title="Select file", filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+    apps.append(filename)
+    print(apps)
+    for app in apps:
+        label = tk.Label(frame, text=app, fg="#8960d8")
+        label.pack()
+
+
+def runApps():
+    for app in apps:
+        os.startfile(app)
+
+
+canvas = tk.Canvas(root, height=700, width=700, bg="#8960d8")
 canvas.pack()
 
-openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#8960d8")
+frame = tk.Frame(root, bg="white")
+frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+
+openFile = tk.Button(root, text="Open File", padx=25,
+                     pady=10, fg='white', bg="#8960d8", command=addApp)
 openFile.pack()
 
-runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#8960d8")
+runApps = tk.Button(root, text="Run Apps", padx=25,
+                    pady=10, fg='white', bg="#8960d8", command=runApps)
 runApps.pack()
+
+for app in apps:
+    label = tk.Label(frame, text=app)
+    label.pack()
+
 
 root.mainloop()
